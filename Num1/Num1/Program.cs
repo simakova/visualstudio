7e9d90689bb636d62979443754f461f9ef9dcb69
Ксения Симакова < >
2016-09-09 16:31:38 +0300

    Num1 diff

--- Num1/Num1/Program.cs
+++ Num1/Num1/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;*/
 
 namespace Num1
 {
@@ -10,16 +10,16 @@ namespace Num1
     {
         static void Main()
         {
-            int sum=0, num;
+            double sum = 0;
             Console.WriteLine("Задача 1924. Четыре чертенка");
             Console.Write("Введите целое число (от 1 до 50): ");
             String n = Console.ReadLine();
-            num = Convert.ToInt32(n);
+            int num = Convert.ToInt32(n);
             for (int i = 0; i <= num; i++)
-                sum = sum + i;
-            if (sum%2 == 1) Console.WriteLine("grimy");
-           // else if (sum == 0) Console.WriteLine("nobody");
-            else Console.WriteLine("Ответ: black");
+                sum =+ i;
+            if (sum%2 == 0) Console.WriteLine("black");
+            else Console.WriteLine("grimy");
+//            Console.ReadKey();
         }
     }
 }
