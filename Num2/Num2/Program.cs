cf3d14d96ec24e354623a5d8c869729e6e612395
kseni <kseni@DESKTOP-5H7AK58>
2016-09-09 20:56:49 +0300

    num2 done

--- Num2/Num2/Program.cs
+++ Num2/Num2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Xml.Schema;
 
 
 namespace Num2
@@ -8,11 +9,36 @@ namespace Num2
     {
         private static void Main()
         {
+            int  a, b, c;
             string[] numbers = new string[3];
             numbers[0] = Console.ReadLine();
             numbers[1] = Console.ReadLine();
             numbers[2] = Console.ReadLine();
-            Console.WriteLine(int.Parse(numbers[0]) - int.Parse(numbers[1]) * int.Parse(numbers[2]));
+
+            a = int.Parse(numbers[0]);
+            b = int.Parse(numbers[1]);
+            c = int.Parse(numbers[2]);
+
+            int count = 9;  
+            int[] res = new int[count];
+            res[0] = a - b * c;
+            res[1] = a - b + c;
+            res[2] = a - b - c;
+            res[3] = a + b - c;
+            res[4] = a + b * c;
+            res[5] = a + b + c;
+            res[6] = a * b - c;
+            res[7] = a * b + c;
+            res[8] = a * b * c;
+
+            int min = res[0];
+            for (int i = 1; i < count; i++)
+            {
+                if (res[i] < min)
+                    min = res[i];
+            }
+
+            Console.WriteLine(min);
         }
     }
 }
