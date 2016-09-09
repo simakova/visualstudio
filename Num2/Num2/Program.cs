88cf724c6b1e55e2272961b937c058a1e5572c12
Ксения Симакова < >
2016-09-09 18:12:44 +0300

    add Num2

---
+++ Num2/Num2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Num2
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            string[] numbers = new string[3];
+            numbers[0] = Console.ReadLine();
+            numbers[1] = Console.ReadLine();
+            numbers[2] = Console.ReadLine();
+            Console.WriteLine(int.Parse(numbers[0]) - int.Parse(numbers[1]) * int.Parse(numbers[2]));
+        }
+    }
+}
+
