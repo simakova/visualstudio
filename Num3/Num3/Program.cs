ce0af951ea1b1237daaa7abea176e2c4bbcfc19d
kseni <kseni@DESKTOP-5H7AK58>
2016-09-10 13:20:53 +0300

    num3 edit

--- Num3/Num3/Program.cs
+++ Num3/Num3/Program.cs
@@ -5,32 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-/* 1. Раскладываем введенное число N на множители делением на простые числа (массив prime) - получаем новый массив digits
+/* 1. Раскладываем заданное число N на множители делением на простые числа (массив prime) - получаем новый массив digits
  2. Циклом перебираем массив digits и максимально сворачиваем перемножением элементов (произведение <=10)
  */
+
 namespace Num3
 {
     public class Program
     {
         private static void Main()
         {
-            int N, Q, count = 4;
+            int N, Q, count = 4; //счетчик массива простых чисел до 10
             int[] prime = new int[] {2, 3, 5, 7};
-            List<int> digits = new List<int>();
+            //List<int> digits = new List<int>(); //динамический массив множителей заданного числа
+            int[] counter = new int[10]; //массив для подсчета количества множителей
 
-            String num = Console.ReadLine();
+            String num = Console.ReadLine(); //вводим строку num и преобразуем ее в число N  
             N = int.Parse(num);
-//1
-            for (int i = 0; i < count; i++)
-            {
-                while (N%prime[i] == 0)
+
+            //1 Цикл for перебирает простые числа из массива prime
+            //2 Цикл while с предусловием, что число N делится без остатка
+            //j - счетчик каждого простого числа, записывается на позицию соответствующую простому числу массива counter
+            // результат = массив counter, содержащий количество простых множителей из которых можно собрать число
+                for (int i = 0; i < count; i++)
                 {
-                    N = N/prime[i];
-                    digits.Add(prime[i]);
+                    int j = 0;
+                    while (N%prime[i] == 0)
+                    {
+                        N = N/prime[i];
+                        //digits.Add(prime[i]);
+                        j++;
+                    }
+                    counter[prime[i]] = j;
+                    if (N == 1) break;
                 }
-                if (N == 1) break;
-            }
-//2
-            }
+           //3 сворачиваем простые числа для уменьшения разрядности искомого числа: 2*2*2=8, 3*3=9, 3*2=6, 2*2=4
+            if (counter[2] != 0)
+                int eight = counter[2] ;
+            counter[prime[eight]] = eight;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # visualstudio - practice (in progress)
 №1 - 1924. Четыре чертенка (Accept)
-![](file:///C:/Users/kseni/Pictures/num1.PNG)
+
 
 №2 - 2066. Простое выражение (Accept)
-![](file:///C:/Users/kseni/Pictures/num2.PNG)
+
 
 №3 - 1014. Произведение цифр
 
